bacd0f68f4aeb39a58e4916806d8f9f546354733
Mohit-Gyansys <doramohitkumar@gmail.com>
2025-07-28 17:36:27 +0530

    config and state defined

---
+++ src/config.py
@@ -0,0 +1,25 @@
+import os
+from dotenv import load_dotenv
+from utils.common import read_json
+from constants import BASE_DIR
+from pathlib import Path
+
+load_dotenv()
+
+
+class ConfigurationManager:
+    def __init__(self):
+        self.config = read_json(Path.joinpath(BASE_DIR, "config/config.json"))
+
+    def get_knowledge_base_config(self):
+        config = self.config["knowledge_base"]
+        return config
+
+    def get_llm_config(self):
+        config = self.config["llm_config"]
+        config["GEMINI_API_KEY"] = os.getenv("GEMINI_API_KEY")
+        return config
+
+    def get_agent_config(self):
+        config = self.config["agent_config"]
+        return config
---
+++ src/llm_config.py
@@ -0,0 +1,32 @@
+from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
+from config import ConfigurationManager
+
+config = ConfigurationManager()
+llm_config = config.get_llm_config()
+
+EMBEDDING_MODEL = llm_config["EMBEDDING_MODEL"]
+GENERATION_MODEL = llm_config["GENERATION_MODEL"]
+LLM_TEMPERATURE = llm_config["LLM_TEMPERATURE"]
+GOOGLE_API_KEY = llm_config["GEMINI_API_KEY"]
+
+def get_gemini_llm():
+    """Initializes and returns the Google Gemini LLM."""
+    return ChatGoogleGenerativeAI(
+        model=GENERATION_MODEL,
+        temperature=LLM_TEMPERATURE,
+        google_api_key=GOOGLE_API_KEY
+    )
+
+def get_gemini_embeddings():
+    """Initializes and returns the Google Gemini Embeddings model."""
+    return GoogleGenerativeAIEmbeddings(
+        model=EMBEDDING_MODEL,
+        google_api_key=GOOGLE_API_KEY
+    )
+
+if __name__ == "__main__":
+    LLM = get_gemini_llm()
+    EMBEDDINGS = get_gemini_embeddings()
+
+    print(LLM)
+    print(EMBEDDINGS)
